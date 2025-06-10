8c60bf6da7465eec34ff3d22c48a750b3de1ea2f
Bulat Safin <BulatSafin83030@gmail.com>
2025-06-09 21:10:04 -0400

    решение модуля 2

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,19 @@
 let calculator = {
-  // ваш код
+  num1: 0,
+  num2: 0,
+
+  read(a, b) {
+    this.num1 = a;
+    this.num2 = b;
+  },
+
+  sum() {
+    return this.num1 + this.num2;
+  },
+
+  mul() {
+    return this.num1 * this.num2;
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
